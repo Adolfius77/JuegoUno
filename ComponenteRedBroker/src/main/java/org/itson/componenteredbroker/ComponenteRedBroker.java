2e27e75f87cc48e064b166c895ea1b99bc29fd0f
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2026-03-13 16:39:08 -0700

    Componente de red / Broker

---
+++ ComponenteRedBroker/src/main/java/org/itson/componenteredbroker/ComponenteRedBroker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.componenteredbroker;
+
+/**
+ *
+ * @author USER
+ */
+public class ComponenteRedBroker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
