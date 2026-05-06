8789d0dab63c5e204920550b2411941426c4f6df
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2026-05-06 01:02:01 -0700

    borre cosas que ya no se ocuparan

--- RED-COMUNICACION/src/main/java/interfaces/IReceptorMensajes.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-import dtos.paqueteRedDTO;
-import Server.ServerProxy;
-
-public interface IReceptorMensajes {
-    void procesarMensaje(paqueteRedDTO paquete, ServerProxy hiloRemintente);
-}
